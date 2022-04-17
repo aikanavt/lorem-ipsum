5af1874bc85afc29f8039bdf0fc36bbd39cc9a0a
aikanavt <sagadiyevaaa@gmail.com>
2022-04-18 00:03:26 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>TIRED OF BORING LOREM IPSUM?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          className="input"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((paragraph) => {
+          return <p>{paragraph}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
